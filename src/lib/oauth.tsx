f338513d6aad1d8801267564ec77e7d1ac21bc49
blessingtutka <blessingtutka298@gmail.com>
2024-08-29 21:23:35 +0200

    feat(auth): add auth user date saving

--- src/lib/oauth.tsx
+++ src/lib/oauth.tsx
@@ -1,19 +1,33 @@
 "use client";
 
-import React, { useMemo, useCallback, createContext } from "react";
+import React, {
+  useMemo,
+  useCallback,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "convex/react";
 import type { Context, PropsWithChildren } from "react";
-
-const redirect_uri = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI as string;
-if (!redirect_uri) throw new Error("Redirect URI  not found");
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import { checkOrCreateUser } from "../../convex/user";
 
 export const MyAuth0Context = createContext(null) as Context<IAuth0 | null>;
 
 export default function MyAuth0Provider({ children }: PropsWithChildren) {
-  const { loginWithRedirect: auth0Login, logout: auth0Logout } = useAuth0();
+  const {
+    loginWithRedirect: auth0Login,
+    logout: auth0Logout,
+    user,
+    isAuthenticated,
+    isLoading,
+  } = useAuth0();
 
   const logout = useCallback(
-    () => auth0Logout({ logoutParams: { returnTo: window.location.origin } }),
+    () =>
+      auth0Logout({ logoutParams: { returnTo: `${window.location.origin}/` } }),
     [auth0Logout]
   );
 
@@ -22,13 +36,13 @@ export default function MyAuth0Provider({ children }: PropsWithChildren) {
       auth0Login({
         authorizationParams: {
           connection: connection,
-          redirect_uri: redirect_uri,
+          redirect_uri: `${window.location.origin}/user`,
         },
       }),
     [auth0Login]
   );
 
-  const auh0 = useMemo(
+  const auth0 = useMemo(
     () =>
       ({
         login,
@@ -36,7 +50,35 @@ export default function MyAuth0Provider({ children }: PropsWithChildren) {
       }) as IAuth0,
     [login, logout]
   );
+
+  const checkOrCreateUserMutation = useMutation(api.user.checkOrCreateUser);
+
+  useEffect(() => {
+    if (!isAuthenticated || !user) {
+      return;
+    }
+
+    const syncUser = async () => {
+      try {
+        if (!user.sub || !user.email) {
+          throw Error("id or email not define");
+        }
+        await checkOrCreateUserMutation({
+          user: {
+            sub: user.sub,
+            email: user.email,
+            name: user.name || "Anomynous",
+          },
+        });
+      } catch (error) {
+        return;
+      }
+    };
+
+    syncUser();
+  }, [isAuthenticated, checkOrCreateUserMutation, user]);
+
   return (
-    <MyAuth0Context.Provider value={auh0}>{children}</MyAuth0Context.Provider>
+    <MyAuth0Context.Provider value={auth0}>{children}</MyAuth0Context.Provider>
   );
 }

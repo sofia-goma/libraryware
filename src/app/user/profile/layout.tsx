d5b0b01ea50f70406d308580816e9a38181a9963
blessingtutka <blessingtutka298@gmail.com>
2024-09-06 13:54:53 +0200

    refactor(profile): get user data

--- src/app/user/profile/layout.tsx
+++ src/app/user/profile/layout.tsx
@@ -1,32 +1,15 @@
 "use client";
 import React from "react";
-import { useMutation } from 'convex/react';
-import { api } from '../../../../convex/_generated/api';
 import ProfileInfoCard from "@/components/ui/ProfileInfoCard";
 import ProfileTabCard from "@/components/ui/ProfileTabCard";
+import { useAuth } from "@/providers/auth-provider";
 
 function ProfileLayout() {
-  // npx convex dev --configure=existing --team librarywave --project libraryware
-  const getUser = useMutation(api.user.getUser);
-
-  const fetchUser = async () => {
-    // setLoading(true);
-    // setError(null);
-    try {
-      // const userData = await getUser({ userId: "hellowd" });
-      // console.log(userData);
-      // setUser(userData);
-    } catch (err) {
-      // setError(err.message);
-    } finally {
-      // setLoading(false);
-    }
-  };
-  // console.log(getUserInfo);
+  const { user } = useAuth();
   return (
-    <div className="flex gap-2">
-      <ProfileInfoCard />
-      <ProfileTabCard />
+    <div className="flex flex-col md:flex-row gap-2">
+      <ProfileInfoCard user={user} />
+      <ProfileTabCard user={user} />
     </div>
   );
 }

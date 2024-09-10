91b0d6751a40ba022caec38e53226325cf35ea24
landbit <landbit@debian.landry.com>
2024-09-10 12:27:18 +0200

    feat: affichages des post

--- src/app/user/forum/page.tsx
+++ src/app/user/forum/page.tsx
@@ -1,9 +1,25 @@
+"use client";
 import PostUI from "@/components/shared/post-ui";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Forum() {
+  const posts = useQuery(api.post.getPosts);
   return (
-    <div className="">
-      <PostUI />
-    </div>
+    <ScrollArea className="w-full h-[80vh] overflow-y-auto">
+      <div className="">
+        {posts?.map((e) => (
+          <PostUI
+            key={e._id}
+            body={e.body}
+            userId={e.userId}
+            time={e._creationTime}
+            bookId={e.bookId}
+            title={e.title}
+          />
+        ))}
+      </div>
+    </ScrollArea>
   );
 }

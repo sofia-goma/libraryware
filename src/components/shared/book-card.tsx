5b8e0c11f16b6d0f7b36247c30c102636e5cb8c2
landbit <landbit@debian.landry.com>
2024-09-10 11:16:12 +0200

    fix: rendre la card cliquable & le titre

--- src/components/shared/book-card.tsx
+++ src/components/shared/book-card.tsx
@@ -66,42 +66,39 @@ export default function BookCard({
 
   return (
     <Card className="w-[250px] p-0 m-0">
-      <CardHeader className="pt-0 text-center">
-        <CardTitle>
-          <Link
-            className="text-sm hover:text-primary transition-colors"
-            href={href}
-          >
+      <Link href={href}>
+        <CardHeader className="p-4 text-center">
+          <CardTitle className="text-sm hover:text-primary transition-colors h-5 overflow-hidden">
             {title}
-          </Link>
-        </CardTitle>
-        <CardDescription>
-          <span className="text-xs">
-            <span className="font-bold">Author: </span>
-            {author}
-          </span>
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="py-0 flex items-center justify-center">
-        <Image
-          src={`${cover ? `${cover}` : `/cover_not_found.jpg`}`}
-          width={150}
-          height={150}
-          className="object-cover"
-          alt="book card image"
-        />
-      </CardContent>
-      <CardFooter className="pb-0 flex items-center justify-center gap-3 py-2">
-        <BookAIcon className="hover:cursor-pointer" color="blue" />
-        <PlusCircleIcon className="hover:cursor-pointer" />
-        <div className="" onClick={bookmark}>
-          {!isBookmarked ? (
-            <Bookmark className="hover:cursor-pointer" />
-          ) : (
-            <BookmarkCheck className="hover:cursor-pointer" />
-          )}
-        </div>
-      </CardFooter>
+          </CardTitle>
+          <CardDescription>
+            <span className="text-xs">
+              <span className="font-bold">Author: </span>
+              {author}
+            </span>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="py-0 flex items-center justify-center">
+          <Image
+            src={`${cover ? `${cover}` : `/cover_not_found.jpg`}`}
+            width={150}
+            height={150}
+            className="object-cover"
+            alt="book card image"
+          />
+        </CardContent>
+        <CardFooter className="pb-0 flex items-center justify-center gap-3 py-2">
+          <BookAIcon className="hover:cursor-pointer" color="blue" />
+          <PlusCircleIcon className="hover:cursor-pointer" />
+          <div className="" onClick={bookmark}>
+            {!isBookmarked ? (
+              <Bookmark className="hover:cursor-pointer" />
+            ) : (
+              <BookmarkCheck className="hover:cursor-pointer" />
+            )}
+          </div>
+        </CardFooter>
+      </Link>
     </Card>
   );
 }

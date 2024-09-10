2b6becb64d163b23c7a823162d8489150e6f79a1
blessingtutka <blessingtutka298@gmail.com>
2024-09-10 22:42:32 +0200

    refactor(user/forum): refactor user forum page

--- src/app/user/forum/page.tsx
+++ src/app/user/forum/page.tsx
@@ -1,26 +1,9 @@
-"use client";
-import PostUI from "@/components/shared/post-ui";
-import { useQuery } from "convex/react";
-import { api } from "../../../../convex/_generated/api";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import Posts from "@/components/post/posts";
 
 export default function Forum() {
-  const posts = useQuery(api.post.getPosts);
   return (
-    <ScrollArea className="w-full h-[80vh] overflow-y-auto">
-      <div className="">
-        {posts?.map((e) => (
-          <PostUI
-            key={e._id}
-            id={e._id}
-            body={e.body}
-            userId={e.userId}
-            time={e._creationTime}
-            bookId={e.bookId}
-            title={e.title}
-          />
-        ))}
-      </div>
-    </ScrollArea>
+    <div className="">
+      <Posts />
+    </div>
   );
 }

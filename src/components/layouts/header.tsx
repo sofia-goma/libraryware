dff084e8b954d92f928f2720db62c98f0d983ab4
BirushaNdegeya <birushandegeya@gmail.com>
2024-09-13 19:54:48 +0200

    feat: add login button to the header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { useContext } from "react";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Logo from "@/components/shared/logo";
 import ModeToggle from "../shared/mode-toggle";
+import { GoogleIcon } from "../icons/google";
+import { AuthContext } from "@/providers/auth-provider";
 
 export default function Header() {
+  const { login } = useContext(AuthContext) as IAuth0;
   return (
     <header className="tracking-wide bg-background z-50 sticky top-0">
       <section className="py-2 bg-primary text-primary-foreground text-center px-10">
@@ -20,9 +24,16 @@ export default function Header() {
 
           {/* <Searchbar /> */}
           <div className="flex items-center gap-3">
-            <Link href="/login">
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => login("google-oauth2")}
+            >
+              <GoogleIcon className="mr-2 h-4 w-4" /> Continue with Google
+            </Button>
+            {/* <Link href="/login">
               <Button>SIGN IN</Button>
-            </Link>
+            </Link> */}
             <div className="hidden md:block">
               <ModeToggle />
             </div>

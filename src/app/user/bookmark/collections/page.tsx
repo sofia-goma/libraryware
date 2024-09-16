ad010bf00ca8898cd199dc17cc4da24779817c2a
land-bit <landrybitege7@gmail.com>
2024-09-16 11:44:45 +0200

    feat: add collectiond books

--- src/app/user/bookmark/collections/page.tsx
+++ src/app/user/bookmark/collections/page.tsx
@@ -3,10 +3,9 @@ import CollectionList from "@/components/bookmark/collection-list";
 import { useAuth } from "@/providers/auth-provider";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-
 export default function Collections() {
   const { user } = useAuth();
-  if (!user) return;
+  if (!user.id) return;
   return (
     <ScrollArea className="w-full" style={{ height: "calc(100vh - 80px)" }}>
       <CollectionList user={user} />

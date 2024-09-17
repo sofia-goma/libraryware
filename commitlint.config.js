22bec344195eae7b263ef4a1d064b158bc8fa2fb
blessingtutka <blessingtutka298@gmail.com>
2024-09-17 09:55:03 +0200

    fix(chore): fix eslint

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+// commitlint.config.js
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+  },
+};

e5be3c52f8c33780970e42dff41938a31f88b4ad
BirushaNdegeya <birushandegeya@gmail.com>
2024-09-19 14:20:18 +0200

    fix: download file in the bookmark

--- src/components/bookmark/file-card.tsx
+++ src/components/bookmark/file-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -6,7 +8,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import socialDate from "@/lib/social-date";
-import { FileText, Trash2 } from "lucide-react";
+import { Download, FileText, Trash2, Loader2 } from "lucide-react";
 import { Id } from "../../../convex/_generated/dataModel";
 import Image from "next/image";
 import PdfIcon from "../icons/pdf";
@@ -35,20 +37,47 @@ export function FileCard({
   type,
   date,
   file,
+  collectionId,
 }: {
   file?: string;
-  id: Id<"collections"> | any;
+  id: Id<"_storage">;
   userId: Id<"users">;
   title?: string;
   type?: "image" | "text" | "word" | "csv" | "pdf";
   date?: number;
   openLibraryId?: string;
+  collectionId: Id<"collections">;
 }) {
   const moveToTrashConvex = useMutation(api.collections.moveToTrash);
-
+  const [loadingIndicator, setLoadingIndicator] = useState(false);
+  const handleDownload = async () => {
+    setLoadingIndicator(true);
+    try {
+      if (file) {
+        const response = await fetch(file);
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `${title}`;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      }
+      return;
+    } catch {
+      setLoadingIndicator(false);
+    } finally {
+      setLoadingIndicator(false);
+    }
+  };
   const moveToTrash = async () => {
     try {
-      await moveToTrashConvex({ userId: userId, collectionId: id });
+      await moveToTrashConvex({
+        userId: userId,
+        storageId: id,
+        collectionId: collectionId,
+      });
       toast({
         title: "Deleted Successfully",
         description: "You have deleted your post successfully.",
@@ -84,43 +113,61 @@ export function FileCard({
             height={100}
           />
         ) : type == "word" ? (
-          <Word className="w-[150px] hover:scale-105 transition-all" />
+          <Word className="w-[150px] hover:scale-105 transition-all fill-primary" />
         ) : type == "csv" ? (
-          <CsvIcon className="w-[150px] hover:scale-105 transition-all" />
+          <CsvIcon className="w-[150px] hover:scale-105 transition-all fill-primary" />
         ) : (
-          <FileText size={150} className="hover:scale-105 transition-all" />
+          <FileText
+            size={150}
+            className="hover:scale-105 transition-all fill-primary"
+          />
         )}
       </CardContent>
       <CardFooter className="flex justify-between">
         <div className="text-xs text-muted-foreground">{`Created : ${socialDate(date || 0)}`}</div>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button
-              variant="link"
-              className="text-destructive flex justify-center items-center gap-1"
-            >
-              <Trash2 className="w-6 h-6" />
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                {`You wanted to send the book "${title}" to the trash, this book will
-                be permanently deleted in 15 days.`}
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter className="gap-6">
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
-                onClick={moveToTrash}
-                className="bg-destructive text-destructive-foreground hover:bg-destructive"
+        <div className="flex items-center justify-end">
+          <Button
+            variant="link"
+            disabled={loadingIndicator}
+            onClick={handleDownload}
+            className="text-primary flex justify-center items-center gap-1"
+          >
+            {loadingIndicator ? (
+              <Loader2 className="w-6 h-6 animate-spin" />
+            ) : (
+              <Download className="w-6 h-6" />
+            )}
+          </Button>
+
+          {/* <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                variant="link"
+                className="text-destructive flex justify-center items-center gap-1"
               >
-                Delete
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+                <Trash2 className="w-6 h-6" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  {`You wanted to send the book "${title}" to the trash, this book will
+                be permanently deleted in 15 days.`}
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter className="gap-6">
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={moveToTrash}
+                  className="bg-destructive text-destructive-foreground hover:bg-destructive"
+                >
+                  Delete
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog> */}
+        </div>
       </CardFooter>
     </Card>
   );
